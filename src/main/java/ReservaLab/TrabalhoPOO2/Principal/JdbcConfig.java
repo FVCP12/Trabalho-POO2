0bf08596a4c3488ae0b9b9056d31b7aec33ec9d9
felipe vieira <fvcp12@gmail.com>
2017-10-13 10:20:19 -0300

    tela inicial

---
+++ src/main/java/ReservaLab/TrabalhoPOO2/Principal/JdbcConfig.java
@@ -0,0 +1,48 @@
+package ReservaLab.TrabalhoPOO2.Principal;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@EnableTransactionManagement
+public class JdbcConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		dataSource.setUrl("jdbc:sqlserver://DESKTOP-FELIPE\\\\SQLEXPRESS:1433;databasename=mvc");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("1234");
+		return dataSource;
+	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+		sessionFactoryBean.setDataSource(dataSource());
+		sessionFactoryBean.setPackagesToScan("ReservaLab.TrabalhoPOO2.*");
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
+		hibernateProperties.put("hibernate.show_sql", true);
+		hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
+		//hibernateProperties.put("hibernate.hbm2ddl.auto", "create");
+		sessionFactoryBean.setHibernateProperties(hibernateProperties);
+
+		return sessionFactoryBean;
+	}
+
+	@Bean
+	public HibernateTransactionManager transactionManager() {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory().getObject());
+		return transactionManager;
+	}
+}
