76679359445d7fe7502db2adeb5c48b09f5fe4b0
taynara539 <taynara.kris@gmail.com>
2017-10-23 22:56:07 -0200

    Alteração do arquivo pom.xml Lembrar de após criar o banco, tirar o comentário da linha UPDATE e comentar o CREATE no pacote MAIN, na classe JDBC

--- src/main/java/ReservaLab/TrabalhoPOO2/Principal/JdbcConfig.java
+++ src/main/java/ReservaLab/TrabalhoPOO2/Principal/JdbcConfig.java
@@ -14,6 +14,40 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 @EnableTransactionManagement
 public class JdbcConfig {
 
+    //Para usar Postgres
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/reservalab");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("123456");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("ReservaLab.TrabalhoPOO2.*");
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        hibernateProperties.put("hibernate.show_sql", true);
+        //hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.put("hibernate.hbm2ddl.auto", "create");
+        sessionFactoryBean.setHibernateProperties(hibernateProperties);
+
+        return sessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+    /*
+
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -44,5 +78,5 @@ public class JdbcConfig {
 		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
 		transactionManager.setSessionFactory(sessionFactory().getObject());
 		return transactionManager;
-	}
+	}*/
 }
--- src/main/java/ReservaLab/TrabalhoPOO2/Principal/TrabalhoPoo2Application.java
+++ src/main/java/ReservaLab/TrabalhoPOO2/Principal/TrabalhoPoo2Application.java
@@ -1,18 +1,18 @@
 package ReservaLab.TrabalhoPOO2.Principal;
 
+import ReservaLab.TrabalhoPOO2.Model.Funcionarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-
-@ComponentScan(basePackages = {"ReservaLab.TrabalhoPOO2.Principal","ReservaLab.TrabalhoPOO2.Model",
-                                "ReservaLab.TrabalhoPOO2.DAO","ReservaLab.TrabalhoPOO2.Controller"})
+@ComponentScan(basePackages = {"ReservaLab.TrabalhoPOO2.Principal", "ReservaLab.TrabalhoPOO2.Model",
+    "ReservaLab.TrabalhoPOO2.DAO", "ReservaLab.TrabalhoPOO2.Controller"})
 @EnableAutoConfiguration
 public class TrabalhoPoo2Application {
 
-	public static void main(String[] args) {
-	    ConfigurableApplicationContext context =
-                    SpringApplication.run(TrabalhoPoo2Application.class);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context
+                = SpringApplication.run(TrabalhoPoo2Application.class);
+    }
 }
